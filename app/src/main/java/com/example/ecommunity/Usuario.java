72e63820f3f5a7d171551b0edb94f585c457154a
JardelPasturczak <jardel.pas@gmail.com>
2019-11-22 13:17:40 -0300

    Classe USUARIO e suas variáveis criadas

---
+++ app/src/main/java/com/example/ecommunity/Usuario.java
@@ -0,0 +1,104 @@
+package com.example.ecommunity;
+
+public class Usuario {
+
+    private int id;
+    private String razaoSocial;
+    private String cnpj;
+    private String nomeRepresentante;
+    private String emailRepresentante;
+    private String telefoneRepresentante;
+    private String cep;
+    private String cidade;
+    private String rua;
+    private String numero;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNomeRepresentante() {
+        return nomeRepresentante;
+    }
+
+    public void setNomeRepresentante(String nomeRepresentante) {
+        this.nomeRepresentante = nomeRepresentante;
+    }
+
+    public String getEmailRepresentante() {
+        return emailRepresentante;
+    }
+
+    public void setEmailRepresentante(String emailRepresentante) {
+        this.emailRepresentante = emailRepresentante;
+    }
+
+    public String getTelefoneRepresentante() {
+        return telefoneRepresentante;
+    }
+
+    public void setTelefoneRepresentante(String telefoneRepresentante) {
+        this.telefoneRepresentante = telefoneRepresentante;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
