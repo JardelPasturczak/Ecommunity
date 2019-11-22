98ba709fc4be73b1d7bde25abb2dba5ae6f8f0e6
JardelPasturczak <jardel.pas@gmail.com>
2019-11-21 21:34:39 -0300

    LOGIN: criação da tela e instanciação das variáveis

--- app/src/main/java/com/example/ecommunity/MainActivity.java
+++ app/src/main/java/com/example/ecommunity/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.ecommunity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_login_usuario, et_login_senha;
+
+    Button bt_login_entrar, bt_login_criarUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_login_usuario = (EditText) findViewById(R.id.et_login_usuario);
+        et_login_senha = (EditText) findViewById(R.id.et_login_senha);
+
+        bt_login_entrar = (Button) findViewById(R.id.bt_login_entrar);
+        bt_login_criarUsuario = (Button) findViewById(R.id.bt_login_criarUsuario);
+
+
     }
 }
